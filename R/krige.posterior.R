656e3f78ffda12e4c94e98337bd2cb67e4fd3261
Jason S. Byers <jaybyers55@gmail.com>
2020-03-11 20:00:02 +0000

    version 0.4.1

--- R/krige.posterior.R
+++ R/krige.posterior.R
@@ -1,16 +1,19 @@
 # CREATING THE LOG-POSTERIOR OBJECTIVE FUNCTION
-krige.posterior <- function(tau2,phi,sigma2,beta,y,X,east,north){ 
+krige.posterior <- function(tau2,phi,sigma2,beta,y,X,east,north,p.spatial.share=0.5,p.range.share=0.5,p.beta.var=10,tot.var=var(y)){ 
   distance <- as.matrix(dist(cbind(east,north), method = "euclidean", diag = TRUE, upper = TRUE) )
+  tau2.shape<-1+1/(1-p.spatial.share)
+  sigma2.shape<-1+1/p.spatial.share
+  phi.shape<-1+(1/p.range.share)
+  phi.rate<-max(distance)
   Sigma <- ifelse(distance>0, sigma2*exp(-(phi^2)*(distance^2)), tau2+sigma2) # GAUSSIAN VAR/COV
   mu<-X%*%beta
   log.det.Sigma <- sum(log(eigen(Sigma)$values))
   chol.Sigma <- chol(Sigma)
   loglik <- -(1/2)*log.det.Sigma - 0.5*t(y-mu)%*%chol2inv(chol.Sigma)%*%(y-mu) 
-  # PRIOR LINE: by proportionality we leave out -(N/2)*log(2) -(N/2)*log(pi) 
-  # define prior on linear coefficients
-  log.mu.prior <- sum(log(dmvnorm(beta, mean=rep(0,length(beta)), sigma = diag(length(beta))*10)))
-  logpost <- loglik-((2*(sigma2^2)+1)/sigma2)-((2*(tau2^2)+1)/tau2)+log.mu.prior
-  # PRIOR LINE: Adds Inverse Gamma(1,1) priors for tau2 and sigma2. log.mu.prior captures the priors on the regrssion coefficients.
-  # PRIOR LINE: Note that we are using a flat prior on phi. PRIOR ON PHI.
+  log.mu.prior <- sum(log(dmvnorm(beta, mean=rep(0,length(beta)), sigma = diag(length(beta))*p.beta.var)))
+  log.tau2.prior <-dinvgamma(tau2,shape=tau2.shape,rate=tot.var,log=T) 
+  log.sigma2.prior <-dinvgamma(sigma2,shape=sigma2.shape,rate=tot.var,log=T)
+  log.phi.prior <- dinvgamma(phi,shape=phi.shape,rate=phi.rate,log=T)
+  logpost <- loglik+log.tau2.prior+log.sigma2.prior+log.phi.prior+log.mu.prior
   return(logpost)
 }
--- R/metropolis.krige.R
+++ R/metropolis.krige.R
@@ -1,25 +1,38 @@
 # SAMPLER
-metropolis.krige <- function(y,X,east,north,mcmc.samples=100,tau2.shape=5000,tau2.rate=10,phi.shape=5,phi.rate=2000,sigma2.shape=1600,sigma2.rate=10,a.tune=10000)  {
+metropolis.krige <- function(y,X,east,north,mcmc.samples=100,spatial.share=0.5,range.share=0.5,beta.var=10,a.tune=10000){
+	
+  #DEFINE STANDING PARAMETERS
   dist.mat <- as.matrix( dist(cbind(east,north), method = "euclidean", diag = TRUE, upper = TRUE) )
   ideo.out <- lm(y ~ X[,-1])
-  s2 <- summary(ideo.out)$coef[,2]^2/3
+  err.var<-var(resid(ideo.out))
+  s.tau2.shape<-1+1/(1-spatial.share)
+  s.sigma2.shape<-1+1/spatial.share
+  s.phi.shape<-1+1/range.share
+  s.phi.rate<-max(dist.mat)
+  s2<-0.05
+  
   # STARTING VALUES
   mcmc.mat <- matrix(NA,nrow=mcmc.samples,ncol=3+ncol(X))
   dimnames(mcmc.mat)[[2]] <- c("tau2","phi","sigma2",dimnames(X)[[2]])
   n.vars <- ncol(X)
   accepted <- 0
   set.seed(runif(1))
-  mcmc.mat[1,] <- c(1,1,1,rep(0,n.vars))
+  mcmc.mat[1,] <- c((1-spatial.share)*err.var,range.share*s.phi.rate,spatial.share*err.var,ideo.out$coef)
+  
   # START OF MARKOV CHAIN
   for (i in 1:(nrow(mcmc.mat)-1))  {
     if(i%%100==0){cat("Iteration ",i,": Nugget=",mcmc.mat[i,1],". Partial sill=",mcmc.mat[i,3],".\n",sep="")}
-    current <- krige.posterior(mcmc.mat[i,1],mcmc.mat[i,2],mcmc.mat[i,3],mcmc.mat[i,4:ncol(mcmc.mat)],y,X,east,north)
-    tau2 <- rgamma(1,shape=tau2.shape,rate=tau2.rate)
-    phi <-  rgamma(1,shape=phi.shape,rate=phi.rate)
-    sigma2 <- rgamma(1,shape=sigma2.shape,rate=sigma2.rate)
-    beta <- mvrnorm(n = 1, mu=mcmc.mat[i,4:ncol(mcmc.mat)], s2*diag(n.vars))
-    candidate <- krige.posterior(tau2,phi,sigma2,beta,y,X,east,north)
-    a.ratio <- (exp(candidate/a.tune)/exp(current/a.tune))^a.tune
+    current <- krige.posterior(mcmc.mat[i,1],mcmc.mat[i,2],mcmc.mat[i,3],mcmc.mat[i,4:ncol(mcmc.mat)],
+    		y,X,east,north,p.spatial.share=spatial.share,p.range.share=range.share,p.beta.var=beta.var,tot.var=err.var)
+    tau2 <- rinvgamma(1,shape=s.tau2.shape,rate=err.var)
+    sigma2 <- rinvgamma(1,shape=s.sigma2.shape,rate=err.var)
+    phi <-  rinvgamma(1,shape=s.phi.shape,rate=s.phi.rate)
+    #beta <- mvrnorm(n = 1, mu=mcmc.mat[i,4:ncol(mcmc.mat)], diag(diag(vcov(ideo.out)))^s2)
+    #beta <- mvrnorm(n = 1, mu=mcmc.mat[i,4:ncol(mcmc.mat)], s2*diag(diag(vcov(ideo.out))))
+    beta <- mvrnorm(n = 1, mu=mcmc.mat[i,4:ncol(mcmc.mat)], s2*vcov(ideo.out))
+    candidate <- krige.posterior(tau2,phi,sigma2,beta,
+    		y,X,east,north,p.spatial.share=spatial.share,p.range.share=range.share,p.beta.var=beta.var,tot.var=err.var)
+    a.ratio <- ifelse(candidate>current,1.1,(exp(candidate/a.tune)/exp(current/a.tune))^a.tune) 
     if (is.na(a.ratio) == TRUE) { print(paste("is.na:",a.ratio)); a.ratio <- 0 }
     if (a.ratio > runif(1)) {
       accepted <- accepted + 1
@@ -30,5 +43,3 @@ metropolis.krige <- function(y,X,east,north,mcmc.samples=100,tau2.shape=5000,tau
   print(paste(accepted,"accepted out of",nrow(mcmc.mat)-1,"generated", round(100*accepted/(nrow(mcmc.mat)-1)), "percent"))
   return(mcmc.mat)
 }
-
-
